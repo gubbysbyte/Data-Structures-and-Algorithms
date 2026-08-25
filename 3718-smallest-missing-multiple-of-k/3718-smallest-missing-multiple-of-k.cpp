e3e342dd39ecae627fdaf44f858ddd8b7dd44954
Kailash Maurya <kailashhmaurya@gmail.com>
2026-08-25 23:29:39 +0530

    Time: 1 ms (47.29%), Space: 23.9 MB (94.59%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-
-        int n = nums.size();
-        vector<int> store(102, -1);
-
-        for(int i = 0; i < store.size(); i++){
-            cout << store[i] << " ";
-        }
-
-        for(int i = 0; i < n; i++){
-            if(nums[i] % k == 0){
-                store[nums[i]/k] = nums[i];
-            }else{
-                continue;
+        uint64_t x[2] = {0, 0};
+        for (auto& n : nums)
+            if (n % k == 0) {
+                int i = n / k - 1;
+                x[i >> 6] |= 1ULL << (i & 63);
             }
-        } 
-        cout << endl;
-        for(int i = 0; i < store.size(); i++){
-            cout << store[i] << " ";
-        }
 
-        for(int i = 1; i < store.size(); i++){
-            if(store[i] == -1){
-                return k * i;
-            }
-        }
-        return -1;
+        int z = x[0] == -1ULL;
+        return (z * 64 + bit_width(++x[z] & -x[z])) * k;
     }
 };
